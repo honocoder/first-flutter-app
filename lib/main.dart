48ab4e90e3c01614dcba482c8ca25379e0dd2d9a
jimymltta <marlettajimy@gmail.com>
2021-10-12 14:54:06 +0200

    added a BottomNavigationBar and some logic in it

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,42 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text("My First Flutter App"),
+            title: const Text("My First Flutter App"),
             centerTitle: true,
+            backgroundColor: Colors.lightGreen,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Colors.green[600],
+            currentIndex: index,
+            onTap: (x) {
+              setState(() {
+                index = x;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                label: "Account",
+                icon: Icon(Icons.account_circle_outlined),
+              ),
+              BottomNavigationBarItem(
+                label: "Infinite",
+                icon: Icon(Icons.all_inclusive_outlined),
+              ),
+            ],
             backgroundColor: Colors.lightGreen,
           ),
         ),
